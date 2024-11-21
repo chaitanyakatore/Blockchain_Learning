e6aa4e743cb91ab96f5518349a5af0be89c09327
CHAITANYA KATORE <106269976+chaitanyakatore@users.noreply.github.com>
2024-11-21 10:56:32 +0530

    Update MyMessanger.sol

--- MyMessanger.sol
+++ MyMessanger.sol
@@ -1,1 +1,25 @@
 // this is the application which will allow to change the message only for the owner who deploy the project
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract MyMessager{
+
+    uint public counter;
+
+    address public owner;
+
+    string public theMessage;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function updateTheMessage(string memory _theMessage) public{
+    if(msg.sender == owner)
+        theMessage = _theMessage;
+        counter++;
+    }
+
+
+}
