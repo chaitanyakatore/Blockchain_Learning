2b3b4416be7511ff4a89e7ed801de694d4d4f80b
CHAITANYA KATORE <106269976+chaitanyakatore@users.noreply.github.com>
2024-12-03 15:22:14 +0530

    Create 01_IntroContract.sol

---
+++ 01_IntroContract.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract MyContract{
+
+    string public name = "Chaitanya Katore";
+}
+
+
+/*
+ SPDX License Identifier:
+ - This is a comment line that specifies the license under which the contract code is released.
+ - It is required in newer versions of Solidity for compatibility with tools like Ethereum package managers.
+ - MIT is a permissive license allowing reuse with minimal restrictions.
+
+ Pragma Directive
+ - This specifies the version of the Solidity compiler to use.
+ - In this case, the contract will compile only with version 0.8.26.
+ - Why important? Solidity frequently updates, and different versions can have syntax and behavior changes.
+
+*/
